8fa1d7d0506714f10103885fc35b835e6937ae90
Axel Vaganay <avaganay@z1r4p4.42lyon.fr>
2023-07-31 14:18:09 +0200

    module 02 start

---
+++ Module_02/ex00/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 13:37:28 by avaganay          #+#    #+#             */
+/*   Updated: 2023/07/31 13:37:44 by avaganay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void )
+{
+    // Fixed a;
+    // Fixed b( a );
+    // Fixed c;
+    
+    // c = b;
+    // std::cout << a.getRawBits() << std::endl;
+    // std::cout << b.getRawBits() << std::endl;
+    // std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
