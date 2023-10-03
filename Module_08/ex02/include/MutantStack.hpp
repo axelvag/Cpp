2ada68180fc52e216cc781ab51a70c0f87158fe0
Axel Vaganay <avaganay@z2r2p5.42lyon.fr>
2023-10-03 15:20:45 +0200

    cpp 08 finish

--- Module_08/ex02/include/MutantStack.hpp
+++ Module_08/ex02/include/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axel <axel@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 20:12:18 by axel              #+#    #+#             */
-/*   Updated: 2023/08/12 20:12:26 by axel             ###   ########.fr       */
+/*   Updated: 2023/10/03 15:16:15 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,36 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <stack>
+#include <deque>
+#include <vector>
 
-template <typename T>
+//class template pour utiliser les operations du conteneur deque
+template <typename T, class container=std::deque<T> >
+class MutantStack: public std::stack<int>
+{
+    
+private:
+    
+public:
+    MutantStack(void) {};
+    ~MutantStack(void) {};
+    MutantStack(MutantStack const &copy) {*this = copy;};
+    MutantStack &operator=(const MutantStack &assigment) {if (this == &assigment) return (*this); return (*this);};
+    typedef typename container::iterator iterator;
 
+    iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    iterator end()
+    {
+        return this->c.end();
+    }
+
+};
 
 #endif
--- Module_08/ex02/src/main.cpp
+++ Module_08/ex02/src/main.cpp
@@ -3,16 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axel <axel@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 14:40:20 by axel              #+#    #+#             */
-/*   Updated: 2023/08/12 19:55:20 by axel             ###   ########.fr       */
+/*   Updated: 2023/10/03 15:16:06 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/whatever.hpp"
+#include "../include/MutantStack.hpp"
+
+void ft_print(std::string str)
+{
+    std::cout << str << std::endl;
+}
+
+void ft_stack(int n, MutantStack<int>    &ms)
+{
+    std::cout << "push " << n << std::endl;
+    ms.push(n);
+    std::cout << ms.top() << std::endl;
+}
 
 int main(void)
 {
+    ft_print("----------SUBJECT TESTS----------");
+
+    MutantStack<int>    mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    std::cout << "Stack" << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    
+    ft_print("----------MY TESTS----------");
+
+    MutantStack<int>    ms;
+    
+    ft_stack(1, ms);
+    ft_stack(2, ms);
+    ms.pop();
+    std::cout << ms.top() << std::endl;
+    
     return (0);
 }
